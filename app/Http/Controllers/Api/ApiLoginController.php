c5048167672b6accf8a3daad57674a8cc77f97bc
Budi Santoso <budisantoso@smartcloud.id>
2017-10-04 10:19:01 +0700

    Deploy new API for login in andorid

--- app/Http/Controllers/Api/ApiLoginController.php
+++ app/Http/Controllers/Api/ApiLoginController.php
@@ -6,9 +6,10 @@
  * Time: 9:37 AM
  */
 namespace App\Http\Controllers\Api;
-use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Classes\AdditionalFunctionClass;
 
 class ApiLoginController extends Controller
 {
@@ -18,22 +19,59 @@ class ApiLoginController extends Controller
      * Created by Budi Santoso
      * Params @username and @password
      */
+
+    private $additionalFunction;
+
+    public function __construct()
+    {
+        $this->additionalFunction = new AdditionalFunctionClass();
+    }
+
     public function validateLogin(Request $request)
     {
-        dd("hallo");
+        $apiName = "VALIDATE_LOGIN";
         $username = $request->input('username');
         $password = sha1($request->input('password'));
+
+        $sendingParams = [
+            'username' => $username,
+            'password' => $password
+        ];
+
+        if(is_null($username)){
+            return $this->additionalFunction->returnApiMessage($apiName, 404, "Missing required parameter username!", json_encode($sendingParams) );
+        }
+
         $activeUser = User::where(['user_username' => $username])->first();
         if(is_null($activeUser)){
-            return "<div class='alert alert-danger'>Username tidak terdaftar</div>";
+            return $this->additionalFunction->returnApiMessage($apiName, 404, "Username not found!", json_encode($sendingParams) );
         }
+
         if($activeUser->user_password != $password){
-            return "<div class='alert alert-danger'>Password salah</div>";
+            return $this->additionalFunction->returnApiMessage($apiName, 401, "Password not match!", json_encode($sendingParams) );
         }
-        $request->session()->put('activeUser', $activeUser);
-        return "
-            <div class='alert alert-success'>Login berhasil!</div>
-            <script> scrollToTop(); movePage(); </script>";
+
+        $data = [
+            'id' => $activeUser->id,
+            'username' => $activeUser->user_username,
+            'identity_number' => $activeUser->user_identity_number,
+            'address' => $activeUser->user_address,
+            'post_code' => $activeUser->user_post_code,
+            'picture' => 'http://localhost/eefish-web/public/uploads/pengguna'.$activeUser->user_picture,
+            'city_id' => $activeUser->user_cities_id,
+            'phone_number' => $activeUser->user_phone_number,
+            'name' => $activeUser->user_full_name,
+            'email' => $activeUser->user_email
+        ];
+
+        $params = [
+            'code' => 302,
+            'description' => 'Login Success!',
+            'data' => $data
+        ];
+
+
+        return response()->json($params);
 
     }
 
